d9a370ae03ab3d6e5581e43df2374b866e081036
iamayushmishraiitr <iamayushmishra1510@gmail.com>
2024-03-19 15:44:04 +0530

    Changed URL

--- Client/src/Pages/Clientlogin.jsx
+++ Client/src/Pages/Clientlogin.jsx
@@ -17,7 +17,7 @@ const ClientLogin = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const res = await axios.post("https://redcliffe-dashboard.vercel.app/client", {
+      const res = await axios.post("http://localhost:3000/client", {
         email: email,
         password: password,
       });
