44465d71623b9f9bc0530f80db2776322577f6eb
iamayushmishraiitr <iamayushmishra1510@gmail.com>
2024-03-20 05:18:17 +0530

    changed Backend

--- Client/src/Pages/clientOrderDetails.jsx
+++ Client/src/Pages/clientOrderDetails.jsx
@@ -30,7 +30,7 @@ const ClientOrderDetails = () => {
       }
     } catch (err) {
      // console.log(err) ;
-      alert("Data not saved ");
+      alert("Please Enter Valid Data");
     }
   };
 
